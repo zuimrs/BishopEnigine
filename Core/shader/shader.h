d4ca11044ef2c3b084e78e5b54614c75414dd54c
zuimrs <zuimrs@gmail.com>
2017-04-25 11:20:10 +0800

    package shader class

--- Core/shader/shader.h
+++ Core/shader/shader.h
@@ -3,8 +3,21 @@
 
 #include <GL/glew.h>
 #include <string>
-std::string ReadFile(const char *);
-GLuint LoadShader(const char *,const char *);
+class Shader
+{
+public:
+    Shader(const char * vertex_path,const char * fragment_path)
+    {
+        this->_programID = loadShader(vertex_path,fragment_path);
+    }
+    void useShader(){glUseProgram(this->_programID);}
+    GLuint getProgramID(){return this->_programID;}
+private:
+    std::string readFile(const char *);
+    void loadShader(const char *,const char *);
+private:
+    GLuint _programID;
+};
 
 
 #endif
